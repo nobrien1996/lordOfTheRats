81bf91fbc62773015071d10d0df1ce89cada8978
Nick O'Brien <nickobriencomposer@gmail.com>
2025-10-29 23:24:19 -0400

    For publish

--- server/contactList.js
+++ server/contactList.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const email = document.getElementById('email').value.trim();
 
     try {
-      const response = await fetch('http://localhost:4000/api/subscribe', {
+      const response = await fetch('http://localhost:443/api/subscribe', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email }),
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -7,6 +9,19 @@ dotenv.config({ path: '../.env' });
 const subscribeRoute = require('./mailing-list/routes/subscribe.js');
 
 const app = express();
+const options = {
+    key: fs.readFileSync('server\certs\private\thelordoftherats.com-PrivateKey.pem'),
+    cert: fs.readFileSync('server\certs\public\thelordoftherats.com-CSR.pem')
+};
+
+https.createServer(options, app).listen(443, () => {
+    console.log('Server is running on https://localhost:443')
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, SSL!');
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,5 +32,5 @@ app.use(( req, res, next ) => {
 });
 app.use('/api', subscribeRoute);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
