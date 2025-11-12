778fc805f8ad168991dc8c488ad062562e518901
Nick O'Brien <nickobriencomposer@gmail.com>
2025-11-12 16:43:05 -0500

    Fixing handlebars links

--- public/handlebars/footer.handlebars.js
+++ public/handlebars/footer.handlebars.js
@@ -3,24 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const template = Handlebars.compile(source);
 
   const context = {
-    contactUrl: "/public/main-pages/contact.html",
+    contactUrl: "public/main-pages/contact.html",
     copyright: "Nick O'Brien 2025",
     socialLinks: [
       {
         url: "https://www.instagram.com/the.lord.of.the.rats/",
-        icon: "/public/logos/instagram-logo.png",
+        icon: "public/logos/instagram-logo.png",
         alt: "Instagram logo",
         class: "pr-5"
       },
       {
         url: "https://www.tiktok.com/@the.lord.of.the.rats96",
-        icon: "/public/logos/tiktok-logo.png",
+        icon: "public/logos/tiktok-logo.png",
         alt: "TikTok logo",
         class: "pr-5"
       },
       {
         url: "https://www.youtube.com/@The_Lord_Of_The_Rats",
-        icon: "/public/logos/youtube-logo.png",
+        icon: "public/logos/youtube-logo.png",
         alt: "YouTube logo",
         class: ""
       }
